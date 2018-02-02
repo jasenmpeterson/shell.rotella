e6d5be017ab07a4b908803b3aecce0a098def046
Jasen Peterson <misc@jasenpeterson.com>
2018-02-02 16:36:46 -0600

    Functionality
    Signficantly streamlined product filter functionality.

--- src/js/questionnaire.js
+++ src/js/questionnaire.js
@@ -4,10 +4,10 @@ let app = new Vue({
     return {
       questions: null,
       questionIndex: 0,
-      products: new Array(),
-      answers: new Array(),
-      currentRecommendations: new Array(),
-      questionAnswered: false
+      products: [],
+      answers: [],
+      recommendations: [],
+      questionAnswered: false,
     }
   },
   created: function () {
@@ -31,22 +31,20 @@ let app = new Vue({
       // make sure the question has been answered before proceeding
 
       if (this.questionAnswered) {
-        this.questionIndex++;
         this.currentProducts = [];
         this.currentProduct = null;
         this.questionAnswered = false;
+        this.storeRecommendations();
+        this.questionIndex++;
       } else {
         this.modalPopUp("Please select an answer before proceeding.");
       }
 
+      // if last question...
+
       if (app.questions.length === this.questionIndex) {
         this.quizComplete();
-      } else if (app.questions.length !== this.questionIndex) {
-        this.answers = [];
       }
-
-      // let heading = document.querySelector(".title");
-      // Splitting.chars(heading);
 
     },
     prev: function () {
@@ -89,16 +87,19 @@ let app = new Vue({
 
         // Push selected answer(s) to answers array
 
-        app.answers.push({
-          questionIndex: app.questionIndex,
-          id: target.dataset.id,
-          question_weight: target.dataset.weight,
-          answer: target.dataset.answer,
-          recommendations: (this.questions[this.questionIndex].acf.question.answers[index].answer.recommendations !== null ? this.questions[this.questionIndex].acf.question.answers[index].answer.recommendations : null)
-        });
+        if (this.questions[this.questionIndex].acf.question.answers[index]) {
 
+          app.answers.push({
+            questionIndex: app.questionIndex,
+            id: target.dataset.id,
+            question_weight: target.dataset.weight,
+            answer: target.dataset.answer,
+            recommendations: (this.questions[this.questionIndex].acf.question.answers[index].answer.recommendations !== null ? this.questions[this.questionIndex].acf.question.answers[index].answer.recommendations : null)
+          });
 
-        this.filterRecommendations(target.dataset.answer);
+        }
+
+        this.filterRecommendations(target.dataset.id);
 
       }
 
@@ -115,86 +116,32 @@ let app = new Vue({
     },
     filterRecommendations: function (eventTarget) {
 
-      var products = document.querySelectorAll(".product");
-
-      // loop through answers which has a recommendations object
-
-      for (var value of this.answers) {
-
-        // loop through recommendations and push the name to currentProducts array
-
-        for (var recommendation of value.recommendations) {
-          this.currentRecommendations.push({
-            answer: value.answer,
-            id: value.id,
-            recommendation: recommendation.post_title
-          });
+      for (let obj of this.answers) {
+        for (let recommendation of obj.recommendations) {
+          document.querySelector("[data-name='" + recommendation.post_title + "'").classList.add("active");
         }
-
       };
 
-      // remove active states
-
-      for (var product of products) {
-        product.classList.remove("active");
-      }
-
-      // loop through currentProducts array and reapply active state to appropriate products
-
-      var currentRecommendation = null;
-
-      for (currentRecommendation of this.currentRecommendations) {
-        var currentNode = document.querySelector("[data-name='" + currentRecommendation.recommendation + "']");
-        currentNode.classList.add("active");
-      }
-
+    },
+    storeRecommendations: function () {
+      this.recommendations[this.questionIndex] = this.answers;
+      this.answers = [];
     },
     updateFilter: function (product) {
 
-      // Thank you!!!! -- https://stackoverflow.com/a/16100446
-      // var arrayOfObjects = [{ a: 1, aa: "two" }];
-      // var elementPos = arrayOfObjects.map(function (x) { return x.a; }).indexOf(1);
-      // var objectFound = arrayOfObjects[elementPos];
+      for (let obj of this.answers) {
+        if (obj.answer === product) {
+          for (let recommendation of obj.recommendations) {
+            document.querySelector("[data-name='" + recommendation.post_title + "'").classList.remove("active");
+          }
+        }
+      };
 
-      // search for recommended product in currentRecommendations array
-
-      var elementPos = this.currentRecommendations.map(function (x) { return x.answer; }).indexOf(product);
-      var objectFound = this.currentRecommendations[elementPos];
-
-      // remove active state from recommended product node
-
-      var currentNode = document.querySelector("[data-name='" + objectFound.recommendation + "']");
-      currentNode.classList.remove("active");
-
-
-      // filter through the currentRecommendations object and return only objects that DO NOT have the specified value
-
-      let newCurrentRecommendationObject = this.currentRecommendations.filter(function (item) {
-        return item.id !== objectFound.id;
+      let updatedFilter = this.answers.filter(function (obj) {
+        return obj.answer !== product;
       });
 
-      // update recommdended product filter
-
-      this.currentRecommendations = newCurrentRecommendationObject;
-
-      // filter through answers object and return only objects that DO NOT have the specified value
-
-      let newAnswerObject = this.answers.filter(function (item) {
-        return item.answer !== objectFound.answer;
-      });
-
-      // update answers object
-
-      this.answers = newAnswerObject;
-
-      // loop through currentProducts array and reapply active state to appropriate products
-
-      var currentRecommendation = null;
-
-      for (currentRecommendation of this.currentRecommendations) {
-        var currentNode = document.querySelector("[data-name='" + currentRecommendation.recommendation + "']");
-        currentNode.classList.add("active");
-      }
+      this.answers = updatedFilter;
 
     },
     quizComplete: function () {
